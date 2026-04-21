c97da7a03ed58f31b710563f2e3cab6fcf160729
BaiNoname <nguyennhan03012016@gmail.com>
2026-04-21 22:55:05 +0700

    update them

---
+++ Services/EventStatusBackgroundService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GameStore.Services
+{
+    public class EventStatusBackgroundService : BackgroundService
+    {
+        private readonly IServiceScopeFactory scopeFactory;
+
+        public EventStatusBackgroundService(IServiceScopeFactory _scopeFactory)
+        {
+            scopeFactory = _scopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    using var scope = scopeFactory.CreateScope();
+                    var eventService = scope.ServiceProvider.GetRequiredService<EventService>();
+                    eventService.RefreshEventStatuses();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EVENT STATUS BACKGROUND ERROR: " + ex.Message);
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+    }
+}
